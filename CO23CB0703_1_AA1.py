41aae71d9c7f5b9e0e98df17ec347e75c6636c88
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-04-21 16:34:39 +0530

    Create CO23CB0703_1_AA1.py

---
+++ CO23CB0703_1_AA1.py
@@ -0,0 +1,11 @@
+def math_operations():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    add = num1 + num2
+    sub = num1 - num2
+    mul = num1 * num2
+    print("The sum of", num1, "and", num2, "is:", add)
+    print("The subtraction of", num2, "from", num1, "is:", sub)
+    print("The multiplication of", num1, "and", num2, "is:", mul)
+    
+math_operations() 
